d4b6d074d4b8837dc182040d42ed14a086d61c1a
justjapann <eduardobessadesouza@gmail.com>
2022-07-25 10:49:41 -0300

    fix bugs

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,16 @@
 import { ApolloServer } from "apollo-server";
 import { schema } from "../graphql/schema";
+<<<<<<< HEAD
 import { connectDatabase } from "./databaseConnection";
+=======
+import { setupDB, getDB } from "./databaseConnection";
+>>>>>>> parent of 2ee61b9 (fixed: apollo server and mongodb)
 
 const server = new ApolloServer({
   schema,
+  context: {
+    setupDB,
+  },
 });
 
 connectDatabase()
