1c02bba2edef4ae2a08971bb661275337f0c7243
justjapann <eduardobessadesouza@gmail.com>
2022-07-28 16:03:01 -0300

    pr: useAuth is null

--- packages/web/src/modules/pages/user/auth/security.ts
+++ packages/web/src/modules/pages/user/auth/security.ts
@@ -1,7 +1,7 @@
 import { Maybe } from "../../../../../../types/src/Maybe";
+import { JWT_SECRET } from "../../../../../../server/src/environment";
 
-const JWT_TOKEN_KEY =
-  "3ceaaad5d75a72fb884d6edecb6be93129336efd510c09cc113cba3f4bd20468e4989873d151f5c0b5e8d93863ca508b95751766e47bd90ddcc51214e9e5d40e";
+const JWT_TOKEN_KEY = JWT_SECRET;
 
 export const getAuthToken = () => localStorage.getItem(JWT_TOKEN_KEY!);
 
